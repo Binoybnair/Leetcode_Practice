ac08f7902bbe3ee43f452a47e0725313e797fc4d
Binoy <bnair3991@gmail.com>
2024-01-02 21:54:34 +0000

    Leetcode Blind 75 day1_commented

---
+++ LC_217_Contains_Duplicate.py
@@ -0,0 +1,11 @@
+#Return True if duplicate is found in an array
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
